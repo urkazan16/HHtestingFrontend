cd93c7704822a80003280f479b7c5c32127a2ff2
Maxim <Maxim@mail>
2024-06-23 18:35:24 +0300

    add testing front update

--- src/test/java/TestAuthorizationUserHHNegative.java
+++ src/test/java/TestAuthorizationUserHHNegative.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class TestAuthorizationUserHHNegative extends ConfTests {
 
     @Test
-    @DisplayName("Проверка авторизации пользователя")
+    @DisplayName("Проверка авторизации пользователя c неправильным паролем")
     public void checkAuthorizationUserButtonSignIn() throws IOException {
         objAuthorizationPage.openMain();
         objAuthorizationPage.completeAuthorizationForm(objDataOptions.getConfEmail(), "12345"); // Вводим логин и пароль
